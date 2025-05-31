1310d92a98a2ffc8375bfb21e867ae97ec41c527
Cedric Kienzler <cedi@users.noreply.github.com>
2025-05-31 19:45:34 +0200

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,38 +1,10 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume';
 
 async function fetchNavbarConfig() {
-  const response = await fetch('https://api.github.com/orgs/SpechtLabs/repos', {
-    headers: {
-      'Accept': 'application/vnd.github+json',
-      'X-GitHub-Api-Version': '2022-11-28',
-    }
-  });
-
-  if (!response.ok) {
-    throw new Error(`GitHub API error: ${response.statusText}`);
-  }
-
-  const repos = await response.json();
-
-  const projectItems = repos
-    .filter(repo =>
-      repo.homepage &&
-      repo.name.toLowerCase() !== 'spechtlabs.github.io'
-    )
-    .map(repo => ({
-      text: repo.name,
-      link: repo.homepage
-    }));
-
   return defineNavbarConfig([
     { text: 'Home', link: '/' },
     { text: 'Blog', link: '/blog/', activeMatch: '/blog/', },
     { text: 'About Me', link: '/about/' },
-    {
-      text: 'Projects',
-      items: projectItems
-    }
-    // { text: 'Blog', link: '/blog/' },
   ]);
 }
 
